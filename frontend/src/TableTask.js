db11c6b2b75696f8599a2649e7ff6460787a020f
Aaishika S Bhattacharya <mail.aaishika@gmail.com>
2022-05-07 02:16:07 +0530

    ♻️ Handle Updates and Deletes

--- frontend/src/TableTask.js
+++ frontend/src/TableTask.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 
 function TableTask(props){
-    const {tasks} = props;
+    const {tasks, onDelete, onUpdate} = props;
 
     return(
         <table>
@@ -14,12 +14,34 @@ function TableTask(props){
                     return(
                         <tr key={task._id}>
                             <td>{task.title}</td>
-                            <td>{task.description}</td>
+                            <DescriptionForm task={task} onUpdate={onUpdate} />
+                            <td><button onClick={() => onDelete(task._id)}>Delete</button></td>
                         </tr>
                     )
                 })
             }
         </table>
+    )
+}
+
+function DescriptionForm(props){
+    const {task, onUpdate} = props;
+
+    const [description, setDescription] = useState(task.description);
+
+    const handleChange = (event) => {
+        setDescription(event.target.value);
+    }
+
+    return(
+        <div>
+            <td>
+                <input type="text" value={description} onChange={(event) => handleChange(event)} />
+            </td>
+            <td>
+                <button onClick={onUpdate({_id: task._id, description: description})}>Update</button>
+            </td>
+        </div>
     )
 }
 
